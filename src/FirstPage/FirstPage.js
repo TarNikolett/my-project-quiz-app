83daf8de2bb4cbcbff6be88e4f6d4919e45c7f55
TarNikolett <you@example.com>
2021-04-15 12:57:00 +0200

    navigation between pages - working

--- src/FirstPage/FirstPage.js
+++ src/FirstPage/FirstPage.js
@@ -1,11 +1,39 @@
 import './FirstPage.css';
+import {useState} from 'react';
+import LoginPage from '../LoginPage/LoginPage';
+import RegisterPage from '../RegisterPage/RegisterPage.js';
 
 export default function FirstPage(){
+  const [showLoginPage, setShowLoginPage] = useState(false);
+  const [showRegisterPage, setShowRegisterPage] = useState(false);
+
+  function handleOnClick (){
+   setShowLoginPage(true);
+  };
+  function handleRegisterOnClick (){
+    setShowRegisterPage(true);
+   };
+
   return (
-    <div className="container">
-    <h1>Legyen Ön is milliomos!</h1>
-    <button>Bejelentkezés</button>
-    <a href="">Nincs még fiókod? Regisztrálj!</a>
+    <>
+    <div>
+      {!showLoginPage && !showRegisterPage &&
+      <div className="container">
+      <h1>Legyen Ön is milliomos!</h1>
+      <button onClick={handleOnClick}>Bejelentkezés</button>
+      <a onClick={handleRegisterOnClick}>Nincs még fiókod? Regisztrálj!</a>
+      </div>
+      }
     </div>
+
+    <div>
+      {showLoginPage && <LoginPage />}
+    </div>
+
+    <div>
+      {showRegisterPage && <RegisterPage />}
+    </div>
+
+    </>
   )
 }
