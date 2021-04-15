636d2c6a28fd39dcf8eb0204e16b7a94aca6cf7f
TarNikolett <you@example.com>
2021-04-15 10:29:22 +0200

    First commit

---
+++ src/FirstPage/FirstPage.js
@@ -0,0 +1,11 @@
+import './FirstPage.css';
+
+export default function FirstPage(){
+  return (
+    <div className="container">
+    <h1>Legyen Ön is milliomos!</h1>
+    <button>Bejelentkezés</button>
+    <a href="">Nincs még fiókod? Regisztrálj!</a>
+    </div>
+  )
+}
