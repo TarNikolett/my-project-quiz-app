9e6da022466dc83741f722e4fe72ab007337d42c
TarNikolett <you@example.com>
2021-06-08 17:33:45 +0200

    score and question numbers OK

--- src/GamePages/Game.js
+++ src/GamePages/Game.js
@@ -5,21 +5,45 @@ export default function Game (props){
   const questions = props.questions;
 
   const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [correctAnswer, setCorrectAnswer] = useState(questions[0].correct_answer);
 
+  const answers = [];
+
+  function createAnswers (){
+    answers.push(questions[index].correct_answer);
+    questions[index].incorrect_answers.map((element) => answers.push(element));
+  };
+  createAnswers();
+  
   function handleNextButtonOnClick (){
     setIndex(index + 1);
+    setCorrectAnswer(questions[index + 1].correct_answer);
   };
+
+  let answer = answers.map((element, index) => (
+    <button 
+      key={index} 
+      onClick={ () => {
+        if(element === correctAnswer){
+          setScore(score + 1)}
+        }
+      }
+      className={correctAnswer === element ? 'answers-button correct' : 'answers-button wrong'}
+    >{element}</button>
+  ))
   
   return (
     <>
       <div className="container">
-        <div className="question-div" dangerouslySetInnerHTML={{__html: questions[index].question}}/>
-        <div className="answer-div">
-          <button className="answer-button">{questions[index].incorrect_answers[0]}</button>
-          <button className="answer-button">{questions[index].incorrect_answers[1]}</button>
-          <button className="answer-button">{questions[index].incorrect_answers[2]}</button>
-          <button className="answer-button">{questions[index].correct_answer}</button>
+        <div className='header-div'>
+        <div>Questions: {index + 1}/20</div>
+        <div>Score: {score}</div>
         </div>
+        <div className="question-div" dangerouslySetInnerHTML={{__html: questions[index].question}}/>
+        <div className='answer-div'>
+      {answer}
+    </div>
         <div className="next-button-div">
           <button className="next-button" onClick={handleNextButtonOnClick}>Next</button>
         </div>
